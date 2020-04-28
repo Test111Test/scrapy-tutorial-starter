31dfb15bf576aa00320c8f1ab50c8d984425f961
NineMan <nelubinmv@inbox.ru>
2020-04-29 00:44:41 +0400

    First json answer and if need write to file run: 'scrapy crawl quotes -o quotes.json'

---
+++ tutorial/spiders/quotes-spyder.py
@@ -0,0 +1,16 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+
+    start_urls = ['http://quotes.toscrape.com']
+
+    def parse(self, response):
+        self.logger.info('hello this my first spider')
+        quotes = response.css('div.quote')
+        for quote in quotes:
+            yield {
+                'text': quote.css('.text::text').get(),
+                'author': quote.css('.author::text').get(),
+                'tags': quote.css('.tag::text').get(),
+            }
